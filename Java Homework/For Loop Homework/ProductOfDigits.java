390e1abc3297f55dd89ba5d6ea6aff32af733795
Harshal <rahulkurrey321@gmail.com>
2026-02-27 12:19:15 +0530

    Add files via upload

---
+++ Java Homework/For Loop Homework/ProductOfDigits.java
@@ -0,0 +1,47 @@
+// Soumik Karmakar - Object Oriented Programming with Java
+// Product of Digits
+
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        int n = 452;
+        int product = 1;
+
+        for(; n != 0; n /= 10) {
+            product *= n % 10;
+        }
+
+        System.out.println("Product: " + product);
+    }
+}
+
+// Output: Product: 40
+
+/* Second Time
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        int n = 452;
+        int product = 1;
+
+        for(; n != 0; n /= 10) {
+            product *= n % 10;
+        }
+
+        System.out.println("Product: " + product);
+    }
+}
+*/
+
+/* Third Time
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        int n = 452;
+        int product = 1;
+
+        for(; n != 0; n /= 10) {
+            product *= n % 10;
+        }
+
+        System.out.println("Product: " + product);
+    }
+}
+*/
